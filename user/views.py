bb0ffd9ca858f53e042e49c7dd3d60c55f2a0515
Kobi <kobiljon.yusupov@gmail.com>
2018-11-30 14:34:17 -0500

    user registration complete, flash success message after registration complete

--- user/views.py
+++ user/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
@@ -6,7 +7,10 @@ def register(request):
 	if request.method == "POST":
 		form = UserCreationForm(request.POST)
 		if form.is_valid():
+			form.save()
 			username = form.cleaned_data.get('username')
+			messages.success(request, f'Account has been created! You can login now')
+			return redirect('event-home')
 	else:
 		form = UserCreationForm()
 	return render(request, 'user/register.html', {'form': form}) 
